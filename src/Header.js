53a8306c87edc9ede79dd984fd5a798b74dafb6d
ge47weq <ge47weq@tum.de>
2024-08-29 23:58:32 +0200

    second stage module 2 progress

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/Users/sumersinghkang/Desktop/Final Project/littleleomon/src/LittleLemon.png" alt = "Little Lemon Logo" />
+    </header>
+  );
+};
+
+export default Header;
